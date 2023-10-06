45913563e89011235256d85b91368a464e11a468
Matthew Reese <awesomepop012@hotmail.com>
2023-10-06 13:09:49 -0700

    Stuff

--- problems/pset1/find-nonrepeating.py
+++ problems/pset1/find-nonrepeating.py
@@ -1,14 +1,14 @@
 #==========================================PROBLEM==========================================
-#You are given a list of integers where every number appears twice EXCEPT for one of them.
-#Find this number and return it from the function
-#Bonus points if you can do it in linear time complexity (if you don't know what this means don't worry about it)
+# You are given a list of integers where every number appears twice EXCEPT for one of them.
+# Find this number and return it from the function
+# Bonus points if you can do it in linear time complexity (if you don't know what this means don't worry about it)
 
 #Example 1:
 #   input: nums = [2,2,1]
 #   output: 1
 # Explanation: 1 is the only number in the array that only appears once
 
-#Example 2: 
+#Example 2:
 #   input: nums = [4,1,2,1,2]
 #   output: 4
 
@@ -17,9 +17,27 @@
 
 #=================================WRITE YOUR ANSWER BELOW==================================
 def singleNumber(nums):
-    answer = set(nums)
+    # O(n), maybe a bit harder to understand though
+    ans = 0
+    for n in nums:
+        ans ^= n
+    return ans
 
-    return #answer here
+def singleNumberSuboptimal(nums):
+    # Mostly optimal method
+    # - average case O(n) but worst case O(n^2)
+    table = {}
+    for n in nums:
+        if n in table:
+            table[n] += 1
+        else:
+            table[n] = 1
+
+    for k,v in table.items():
+        if v == 1:
+            return k
+
+    return None
 
 
 
--- problems/pset1/num-misc.py
+++ problems/pset1/num-misc.py
@@ -1,36 +1,30 @@
 #This problem has multiple steps of increasing difficulty. Complete as many as you can
 
 #This is the value that will be passed to each function
-num = 20
+num = 100
 
 
 #PROBLEM 1: return a list of every number from 0 to n
 #Ex: n = 10
 #output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 def problem1(n):
-    answer = range(n + 1)
-
-    return answer
+    return range(n + 1)
 
 #PROBLEM 2: return every even number from 0 to n
 def problem2(n):
-    answer = range(0, n + 1, 2)
-    # OR
-    #answer = [for e in range(n + 1) if e % 2 == 0]
-
-    return answer
+    return range(0, n + 1, 2)
 
 #PROBLEM 3: return every even number from 0 to n, NOT including multiples of 4
 def problem3(n):
-    answer = [e for e in range(0, n + 1, 2) if e % 4 != 0]
-
-    return answer
+    return [e for e in range(0, n + 1, 2) if e % 4 != 0]
 
 #PROBLEM 4: return every prime number from 1 to n
 def problem4(n):
+    # O(n^2)
+    # Can definitely be otimized with some smart math, 
+    # but I don't feel like thinking about it
     answer = range(1, n + 1)
 
-    # O(n^2) time complexity. Can likely be optimized
     answer = [e for e in answer for f in answer if e % f != 0]
 
     return answer
@@ -46,10 +40,18 @@ def problem5(n):
 #(this function should only return 1 value, not a list)
 #WARNING: Entering a number larger than 30 may take a very long time to calculate
 #Bonus points if you can make it calculate the 100th number in less than a second (hint: Memoization)
-def problem6(n):
-    #Your code here
+def problem6(n, saved={}):
+    val = 0
+    if n in saved:
+        return saved[n]
 
-    return 0
+    if n <= 2:
+        val = 1
+    else:
+        val = problem6(n - 1, saved) + problem6(n - 2, saved)
+
+    saved[n] = val
+    return val
 
 #PROBLEM 7: return ANY array containing n unique integers that add up to 0
 #Ex: n = 5
@@ -57,8 +59,10 @@ def problem6(n):
 #the arrays [-5,-1,1,2,3] and [-3,-1,2,-2,4] would also be accepted
 #the array [0, 0, 0, 0, 0] would not be accepted because the values aren't unique
 def problem7(n):
-    answer = []
-    #Your code here
+    answer = list(range(-n // 2 + 1, 0)) + list(range(1, n // 2))
+
+    if len(answer) < n:
+        answer += [0];
 
     return answer
 
